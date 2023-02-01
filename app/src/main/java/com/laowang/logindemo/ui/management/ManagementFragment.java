ec5fd16efa2a3fa8a7b86edb3125087e31215488
平平无奇的小老头儿 <31147133+ppwqdxlte@users.noreply.github.com>
2023-02-01 14:23:30 +0800

    note: 生命周期的思考
    ViewModel是伴随整个运行期间的，而Activity每次从其它页面跳到该页面都会重新创建！所以稳定的数据在ViewModel中获得即可，而可能改变的数据最好在onCreateView()方法中刷新

--- app/src/main/java/com/laowang/logindemo/ui/management/ManagementFragment.java
+++ app/src/main/java/com/laowang/logindemo/ui/management/ManagementFragment.java
@@ -1,24 +1,34 @@
 package com.laowang.logindemo.ui.management;
 
+import android.content.res.loader.ResourcesProvider;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.laowang.logindemo.R;
 import com.laowang.logindemo.databinding.FragmentManagementBinding;
 import com.laowang.logindemo.util.ResourceProvider;
 
-public class ManagementFragment extends Fragment {
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
+public class ManagementFragment extends Fragment {
+//    private static AtomicInteger count = new AtomicInteger(0);
     private FragmentManagementBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+//        Log.e("管理页面","创建了"+count.incrementAndGet()+"次"); // 每次从其它页面切换到这个页面都会重新创建一次
         // 初始化 ResourceProvider 的上下文，注意，必须要在ManagementViewModel构造方法之前执行
         ResourceProvider.setmCtx(getContext());
         // 初始化 ManagementViewModel对象
@@ -27,11 +37,24 @@ public class ManagementFragment extends Fragment {
         /* 充气 */
         binding = FragmentManagementBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-        /* 选项页的文字视图，TODO 把文字替换成布局，1.按权限级别显示用户列表（序号，名称，级别，创建时间），1-全部，0-自己；
+        /* 选项页的文字视图，TODO 设置ViewModel的userList
                      TODO 2.按权限显示页面底部操作tab页签 点什么就打开对应的fragment */
         final TextView textView = binding.textManagement;
-        // 视图模型能拿出来文字就会填充进 文本视图XML里！
+        final TableLayout userList = binding.userList;
+        /* 视图模型 有数据的话 就会填充进 文本视图XML里！ */
         viewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        viewModel.getTableRows().observe(getViewLifecycleOwner(), new Observer<List<TableRow>>() {
+            @Override
+            public void onChanged(List<TableRow> tableRows) {
+                // 保留表头
+                if (userList.getChildCount()>1){ //不确定是否直接子view就是TableRow...
+                    userList.removeViews(1,userList.getChildCount() - 1);//不确定是否从0开始
+                }
+                for (TableRow tableRow : tableRows) {
+                    userList.addView(tableRow);
+                }
+            }
+        });
         return root;
     }
 
--- app/src/main/java/com/laowang/logindemo/ui/management/ManagementViewModel.java
+++ app/src/main/java/com/laowang/logindemo/ui/management/ManagementViewModel.java
@@ -1,32 +1,45 @@
 package com.laowang.logindemo.ui.management;
 
+import android.util.Log;
+import android.widget.GridView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 视图模型应该处理数据有关，management页面肯定不止文字啦！还有 grid列表，底部tab页签，每个tab页还要包含 输入框 和 按钮！！
  */
 public class ManagementViewModel extends ViewModel {
-
-    /**
-     * 可变数据：文本 TODO 还有可变用户列表，可变 tabs啊，可变 页面啊啥的。。。？？
-     */
+//    private static AtomicInteger count = new AtomicInteger(0);
     private final MutableLiveData<String> mText;
+    /**
+     * 用户列表
+     */
+    private final MutableLiveData<List<TableRow>> mTableRows;
 
     public ManagementViewModel() {
-        // TODO 在这里设置 页面的数据[ grid, tabs, editTexts, buttons]
+        /* Log.e("管理视图模型","创建了"+count.incrementAndGet()+"次。"); 始终 只有 1 次，说明页面生命周期中视图模型对象只会创建一次，
+        如果只在构造方法获得数据，那么app运行期间都不会得到刷新！所以比如列表的获取，应该在 activity中查询，每次进入页面都会得到刷新！！！ */
         mText = new MutableLiveData<>();
         mText.setValue("This is gallery fragment");
+        mTableRows = new MutableLiveData<>();
     }
 
-    /**
-     * @return 获取文字
-     */
     public LiveData<String> getText() {
         return mText;
     }
-    /*
-    TODO：获取grid? tabs??
-     */
+
+    public LiveData<List<TableRow>> getTableRows() {
+        return mTableRows;
+    }
+
 }
