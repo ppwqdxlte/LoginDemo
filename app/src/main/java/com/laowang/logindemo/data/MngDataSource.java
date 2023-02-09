a469cd456104da806839c7f4a245461cb37bed05
平平无奇的小老头儿 <31147133+ppwqdxlte@users.noreply.github.com>
2023-02-09 12:55:22 +0800

    feat: 修改用户

--- app/src/main/java/com/laowang/logindemo/data/MngDataSource.java
+++ app/src/main/java/com/laowang/logindemo/data/MngDataSource.java
@@ -35,11 +35,11 @@ public class MngDataSource {
         Result<String>[] result = restfulApiHandler.postSync(null, strUrl, null);
         while (result[0] == null) {
         }
-        if (result[0] instanceof Result.Success){
+        if (result[0] instanceof Result.Success) {
             Result.Success<String> success = (Result.Success<String>) result[0];
             String jsonStr = success.getData();
             com.laowang.logindemo.apientity.Result fromJson = new Gson().fromJson(jsonStr, com.laowang.logindemo.apientity.Result.class);
-            if (fromJson.getData() == null){
+            if (fromJson.getData() == null) {
                 return users;
             }
             // 列表的话是List<LinkedTreeMap>类型
@@ -60,53 +60,82 @@ public class MngDataSource {
     public Result<ManagedUser> createUser(String newUsername, String newPwd, int checkedRadioButtonId) {
         ManagedUser managedUser = null;
         String strUrl = ApiUrl.API_BASE + ApiUrl.API_CREATE_USER;
-        Map<String,String> params = new HashMap<>();
-        params.put("username",newUsername);
-        params.put("password",newPwd);
-        params.put("permisstionIndex",checkedRadioButtonId+"");
+        Map<String, String> params = new HashMap<>();
+        params.put("username", newUsername);
+        params.put("password", newPwd);
+        params.put("permisstionIndex", checkedRadioButtonId + "");
         Result<String>[] result = restfulApiHandler.postSync(null, strUrl, params);
         while (result[0] == null) {
         }
-        if (result[0] instanceof Result.Success){
+        if (result[0] instanceof Result.Success) {
             Result.Success<String> success = (Result.Success<String>) result[0];
             String jsonStr = success.getData();
             com.laowang.logindemo.apientity.Result fromJson = new Gson().fromJson(jsonStr, com.laowang.logindemo.apientity.Result.class);
-            if (fromJson.getData() == null){
+            if (fromJson.getData() == null) {
                 return new Result.Error("Fail to create user.", R.string.result_fail_create_user);
             }
             LinkedTreeMap treeMap = (LinkedTreeMap) (fromJson.getData());
-            managedUser = new ManagedUser( treeMap.get("username").toString(), treeMap.get("password").toString(), treeMap.get("level").toString());
+            managedUser = new ManagedUser(treeMap.get("username").toString(), treeMap.get("password").toString(), treeMap.get("level").toString());
             managedUser.setDate(treeMap.get("date").toString());
-            return new Result.Success<ManagedUser>(managedUser,"Create user successfully!",R.string.result_success_create_user);
-        } else if (result[0] instanceof Result.Error){
-            return (Result.Error)result[0];
+            return new Result.Success<ManagedUser>(managedUser, "Create user successfully!", R.string.result_success_create_user);
+        } else if (result[0] instanceof Result.Error) {
+            return (Result.Error) result[0];
         } else {
-            return (Result.Problem)result[0];
+            return (Result.Problem) result[0];
         }
     }
 
     public Result<ManagedUser> deleteUser(String selectedName) {
         ManagedUser managedUser = null;
         String strUrl = ApiUrl.API_BASE + ApiUrl.API_DELETE_USER;
-        Map<String,String> params = new HashMap<>();
-        params.put("selectedUser",selectedName);
+        Map<String, String> params = new HashMap<>();
+        params.put("selectedUser", selectedName);
         Result<String>[] result = restfulApiHandler.postSync(null, strUrl, params);
         while (result[0] == null) {
         }
-        if (result[0] instanceof Result.Success){
+        if (result[0] instanceof Result.Success) {
             Result.Success<String> success = (Result.Success<String>) result[0];
             String jsonStr = success.getData();
             com.laowang.logindemo.apientity.Result fromJson = new Gson().fromJson(jsonStr, com.laowang.logindemo.apientity.Result.class);
-            if (fromJson.getData() == null){
+            if (fromJson.getData() == null) {
                 return new Result.Error("Fail to delete user.", R.string.result_fail_delete_user);
             }
             LinkedTreeMap treeMap = (LinkedTreeMap) (fromJson.getData());
-            managedUser = new ManagedUser( treeMap.get("username").toString(), null, null);
-            return new Result.Success<ManagedUser>(managedUser,"Delete user successfully!",R.string.result_success_delete_user);
-        } else if (result[0] instanceof Result.Error){
-            return (Result.Error)result[0];
+            managedUser = new ManagedUser(treeMap.get("username").toString(), null, null);
+            return new Result.Success<ManagedUser>(managedUser, "Delete user successfully!", R.string.result_success_delete_user);
+        } else if (result[0] instanceof Result.Error) {
+            return (Result.Error) result[0];
         } else {
-            return (Result.Problem)result[0];
+            return (Result.Problem) result[0];
+        }
+    }
+
+    public Result<ManagedUser> modifyUser(String selectedName, String oldPwd, String newPwd, String repeatPwd) {
+        ManagedUser managedUser = null;
+        String strUrl = ApiUrl.API_BASE + ApiUrl.API_PASSWORD;
+        Map<String, String> params = new HashMap<>();
+        params.put("username", selectedName);
+        params.put("oldPwd", oldPwd);
+        params.put("newPwd01", newPwd);
+        params.put("newPwd02", repeatPwd);
+        Result<String>[] result = restfulApiHandler.postSync(null, strUrl, params);
+        while (result[0] == null) {
+        }
+        if (result[0] instanceof Result.Success) {
+            Result.Success<String> success = (Result.Success<String>) result[0];
+            String jsonStr = success.getData();
+            com.laowang.logindemo.apientity.Result fromJson = new Gson().fromJson(jsonStr, com.laowang.logindemo.apientity.Result.class);
+            if (fromJson.getData() == null) {
+                return new Result.Error("Fail to modify user.", R.string.result_fail_modify_user);
+            }
+            LinkedTreeMap treeMap = (LinkedTreeMap) (fromJson.getData());
+            managedUser = new ManagedUser(treeMap.get("username").toString(), treeMap.get("password").toString(), treeMap.get("level").toString());
+            managedUser.setDate(treeMap.get("date").toString());
+            return new Result.Success<ManagedUser>(managedUser, "Modify user successfully!", R.string.result_success_modify_user);
+        } else if (result[0] instanceof Result.Error) {
+            return (Result.Error) result[0];
+        } else {
+            return (Result.Problem) result[0];
         }
     }
 }
