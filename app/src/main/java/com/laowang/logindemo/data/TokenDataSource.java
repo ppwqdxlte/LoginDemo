c0e1f509a8923fd4d04f228048a3d1830eb67e7a
平平无奇的小老头儿 <31147133+ppwqdxlte@users.noreply.github.com>
2023-02-14 17:10:42 +0800

    fix: 修复TokenType错误代码

--- app/src/main/java/com/laowang/logindemo/data/TokenDataSource.java
+++ app/src/main/java/com/laowang/logindemo/data/TokenDataSource.java
@@ -17,13 +17,14 @@ public class TokenDataSource<T> {
 
     private RestfulApiHandler apiHandler = new RestfulApiHandler();
 
+    private final String QTBTAMS = ApiUrl.API_BASE + ApiUrl.API_TYPE_METER;
+
     public List<BaseToken<T>> queryTokenByTypeAndMeterStr(T type, String meterStr) {
         List<BaseToken<T>> tokens = new ArrayList<>();
-        String strUrl = ApiUrl.API_BASE + ApiUrl.API_TYPE_METER;
         Map<String, String> params = new HashMap<>();
-        params.put("tokenType", type.getClass().getSimpleName());
+        params.put("tokenType", type == TokenType.KCT ? "KCT" : "TCC");
         params.put("meterStr", meterStr);
-        Result<String>[] result = apiHandler.postSync(null, strUrl, params);
+        Result<String>[] result = apiHandler.postAsync(null, QTBTAMS, params);
         while (result[0] == null) {
         }
         if (result[0] instanceof Result.Success) {
@@ -39,23 +40,28 @@ public class TokenDataSource<T> {
             // 列表的话是List<LinkedTreeMap>类型
             List<LinkedTreeMap> data = (ArrayList<LinkedTreeMap>) fromJson.getData();
             // batchNo, meterNo, token, date
-            for (int i = 0; i < data.size(); i++) {
-                LinkedTreeMap treeMap = data.get(i);
-                BaseToken token = null;
-                if (type == TokenType.KCT) {
+            if (type == TokenType.KCT){
+                for (int i = 0; i < data.size(); i++) {
+                    LinkedTreeMap treeMap = data.get(i);
+                    BaseToken token = null;
                     token = new KCT(
                             treeMap.get("batchNo").toString(),
                             treeMap.get("meterNo").toString(),
                             treeMap.get("token").toString(),
                             treeMap.get("date").toString());
-                } else {
+                    tokens.add(token);
+                }
+            } else {
+                for (int i = 0; i < data.size(); i++) {
+                    LinkedTreeMap treeMap = data.get(i);
+                    BaseToken token = null;
                     token = new TCC(
                             treeMap.get("batchNo").toString(),
                             treeMap.get("meterNo").toString(),
                             treeMap.get("token").toString(),
                             treeMap.get("date").toString());
+                    tokens.add(token);
                 }
-                tokens.add(token);
             }
         }
         return tokens;
