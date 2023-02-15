d398425ef427b054308d5f5acac99c6b33ef4acf
平平无奇的小老头儿 <31147133+ppwqdxlte@users.noreply.github.com>
2023-02-15 18:34:35 +0800

    fix: 代码重构修复3次崩溃的魔咒

--- app/src/main/java/com/laowang/logindemo/data/TokenRepository.java
+++ app/src/main/java/com/laowang/logindemo/data/TokenRepository.java
@@ -1,11 +1,12 @@
 package com.laowang.logindemo.data;
 
+import androidx.fragment.app.Fragment;
+
 import com.laowang.logindemo.R;
-import com.laowang.logindemo.data.model.BaseToken;
 import com.laowang.logindemo.data.model.KCT;
 import com.laowang.logindemo.data.model.TCC;
 import com.laowang.logindemo.data.model.TokenType;
-import com.laowang.logindemo.util.ResourceProvider;
+import com.laowang.logindemo.ui.MyViewModel;
 
 import java.util.HashMap;
 import java.util.List;
@@ -16,43 +17,54 @@ public class TokenRepository {
     private static volatile TokenRepository instance;
 
     private TokenDataSource dataSource;
-    /**
-     * 缓存KCT类型的tokens，key-表号，value-令牌
-     */
-    private Map<String, List<KCT>> kctMap = new HashMap<>();
-    /**
-     * 缓存TCC类型的tokens，key-表号，value-令牌
-     */
-    private Map<String, List<TCC>> tccMap = new HashMap<>();
+    private MyViewModel myViewModel;
 
-    private TokenRepository(TokenDataSource dataSource) {
-        this.dataSource = dataSource;
+    public TokenDataSource getDataSource() {
+        return dataSource;
     }
 
-    public static TokenRepository getInstance(TokenDataSource dataSource) {
+    public MyViewModel getMyViewModel() {
+        return myViewModel;
+    }
+
+    private Map<String, List<KCT>> kctMap = new HashMap<>();
+
+    private Map<String, List<TCC>> tccMap = new HashMap<>();
+
+    private TokenRepository(TokenDataSource dataSource, Fragment fragment) {
+        this.dataSource = dataSource;
+        this.myViewModel = this.dataSource.getMyViewModel();
+        this.myViewModel.getmKcts().observe(fragment.getViewLifecycleOwner(), kcts -> {
+            if (kcts == null) return;
+            if (kcts.size() > 0) {
+                kctMap.put(this.meterStr, kcts);
+                this.myViewModel.setmKctResult(new Result.Success<List<KCT>>(kcts, "查到了", R.string.result_success_kct_meter_str));
+            } else {
+                this.myViewModel.setmKctResult(new Result.Error("没查到", R.string.result_fail_kct_meter_str));
+            }
+        });
+        this.myViewModel.getmTccs().observe(fragment.getViewLifecycleOwner(), tccs -> {
+            if (tccs == null) return;
+            if (tccs.size() > 0) {
+                tccMap.put(this.meterStr, tccs);
+                this.myViewModel.setmTccResult(new Result.Success<List<TCC>>(tccs, "查到了", R.string.result_success_tcc_meter_str));
+            } else {
+                this.myViewModel.setmTccResult(new Result.Error("没查到", R.string.result_fail_tcc_meter_str));
+            }
+        });
+    }
+
+    public static TokenRepository getInstance(TokenDataSource dataSource, Fragment fragment) {
         if (instance == null) {
-            instance = new TokenRepository(dataSource);
+            instance = new TokenRepository(dataSource, fragment);
         }
         return instance;
     }
 
-    /**
-     * 不确定表号对应的token类型
-     *
-     * @param meterNumber 表号
-     * @return 是否缓存了表号拥有的Tokens
-     */
     public boolean isCachedTokens(String meterNumber) {
         return kctMap.containsKey(meterNumber) || tccMap.containsKey(meterNumber);
     }
 
-    /**
-     * 确定表号对应的token类型
-     *
-     * @param meterNumber 表号
-     * @param tokenType   token类型
-     * @return 是否缓存了表号拥有的Tokens
-     */
     public boolean isCachedTokens(String meterNumber, TokenType tokenType) {
         if (tokenType == TokenType.KCT) {
             return kctMap.containsKey(meterNumber);
@@ -71,39 +83,17 @@ public class TokenRepository {
         return tccMap;
     }
 
-    /* 以下均为业务代码 */
+    private String meterStr;
 
-    /**
-     * 对查询结果进行缓存,装饰成功码或失败码
-     *
-     * @param meterStr 表号
-     * @return 携带token列表的结果
-     */
-    public Result<List<KCT>> queryKctsByMeterStr(String meterStr) {
-        // handle query
-        List<KCT> list = dataSource.queryTokenByTypeAndMeterStr(TokenType.KCT, meterStr);
-        if (list.size() > 0) {
-            // cache the result
-            kctMap.put(meterStr, list);
-            return new Result.Success<List<KCT>>(list, "查到了", R.string.result_success_kct_meter_str);
-        }
-        return new Result.Error("没查到", R.string.result_fail_kct_meter_str);
+    public void queryKctsByMeterStr(String meterStr) {
+        this.meterStr = meterStr;
+        dataSource.queryTokenByTypeAndMeterStr(TokenType.KCT, meterStr);
+//        Result<List<KCT>>
     }
 
-    /**
-     * 对查询结果进行缓存,装饰成功码或失败码
-     *
-     * @param meterStr 表号
-     * @return 携带token列表的结果
-     */
-    public Result<List<TCC>> queryTccsByMeterStr(String meterStr) {
-        // handle query
-        List<TCC> list = dataSource.queryTokenByTypeAndMeterStr(TokenType.TCC, meterStr);
-        if (list.size() > 0) {
-            // cache the result
-            tccMap.put(meterStr, list);
-            return new Result.Success<List<TCC>>(list, "查到了", R.string.result_success_tcc_meter_str);
-        }
-        return new Result.Error("没查到", R.string.result_fail_tcc_meter_str);
+    public void queryTccsByMeterStr(String meterStr) {
+        this.meterStr = meterStr;
+        dataSource.queryTokenByTypeAndMeterStr(TokenType.TCC, meterStr);
+//        Result<List<TCC>>
     }
 }
